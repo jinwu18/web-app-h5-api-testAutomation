f044a800b3335693b5ab71cf01e1c002285f7500
jinwu18 <jinwu18@139.com>
2020-04-20 14:16:46 +0800

    update ApiCommon.java

--- src/main/java/com/automation/common/ApiCommon.java
+++ src/main/java/com/automation/common/ApiCommon.java
@@ -2,6 +2,13 @@ package com.automation.common;
 
 import com.automation.framework.AbastractBase;
 
+import static io.restassured.RestAssured.*;
+import static io.restassured.RestAssured.given;
+
+import io.restassured.RestAssured;
+import io.restassured.config.SSLConfig;
+import io.restassured.response.*;
+import java.util.List;
 
 /**
  * api公共方法类
@@ -9,4 +16,76 @@ import com.automation.framework.AbastractBase;
  */
 public class ApiCommon extends AbastractBase{
 	//api common method to create
+	
+	String stfTokenValue = "Bearer YOUR-TOKEN-HERE";//登录token
+	String stfTokenName = "Authorization";//登录token名称
+	String stfContentType = "application/json";
+	
+	static {
+		RestAssured.baseURI = "http://mystf.org";		
+	}
+	
+	/**
+	 * check device status
+	 * @author: 爱吃苹果的鱼
+	 * @param stfUrl
+	 * @param udid
+	 * @return
+	 */
+	public boolean deviceCanUse(String udid) {
+		Response response = given()
+                //ssl configure all hosts are support 
+                .config((RestAssured.config().sslConfig(new SSLConfig().relaxedHTTPSValidation())))
+                .header(stfTokenName, stfTokenValue)
+                .contentType(stfContentType)
+                .get("/api/v1/devices/" + udid);
+		response.prettyPrint();
+		return response.jsonPath().get("using");
+	}
+	
+	/**
+	 * get devices list
+	 * @author: 爱吃苹果的鱼
+	 * @param stfUrl
+	 * @return
+	 */
+	public List<String> getDevices() {
+		Response response = given()
+				.config(RestAssured.config().sslConfig(new SSLConfig().relaxedHTTPSValidation()))
+				.header(stfTokenName, stfTokenValue)
+				.contentType(stfContentType)
+				.get("/api/v1/devices");
+		response.prettyPrint();
+		return response.jsonPath().getList("serial");
+	}
+	
+	/**
+	 * current device using user
+	 * @author: 爱吃苹果的鱼
+	 * @param udid
+	 * @return
+	 */
+	public String deviceUsingUser(String  udid) {
+		Response response = given()
+				.config(RestAssured.config().sslConfig(new SSLConfig().relaxedHTTPSValidation()))
+				.header(stfTokenName, stfTokenValue)
+				.contentType(stfContentType)
+				.get("/api/v1/user");
+		response.prettyPrint();
+		return response.toString();
+	}
+	
+	/**
+	 * disconnect device
+	 * @author: 爱吃苹果的鱼   
+	 * @param udid
+	 */
+	public void disConnect(String udid) {
+		Response response = given()
+				.config(RestAssured.config().sslConfig(new SSLConfig().relaxedHTTPSValidation()))
+				.header(stfTokenName, stfTokenValue)
+				.contentType(stfContentType)
+				.get("api/v1/user/devices/" + udid);
+		response.prettyPrint();
+	}
 }
--- src/test/java/auto/api/script/TestCaseOfApiParallel.java
+++ src/test/java/auto/api/script/TestCaseOfApiParallel.java
@@ -7,9 +7,6 @@ import org.testng.annotations.Test;
 import com.automation.exception.FrameworkException;
 import com.automation.framework.WebTestNGBase;
 
-import io.restassured.RestAssured;
-import io.restassured.config.SSLConfig;
-
 import static io.restassured.RestAssured.given;
 import io.restassured.response.*;
 
@@ -23,8 +20,6 @@ public class TestCaseOfApiParallel extends WebTestNGBase{
 			boolean result = false;
 			
 	        Response response = given()
-	                // 配置SSL 让所有请求支持所有的主机名
-	                .config((RestAssured.config().sslConfig(new SSLConfig().relaxedHTTPSValidation())))
 	                .param("longitude", 121.04925573429551)
 	                .param("latitude", 31.315590522490712)
 	                .get("https://ditu.amap.com/service/regeo");
@@ -48,8 +43,6 @@ public class TestCaseOfApiParallel extends WebTestNGBase{
 			boolean result = false;
 			
 	        Response response = given()
-	                // 配置SSL 让所有请求支持所有的主机名
-	                .config((RestAssured.config().sslConfig(new SSLConfig().relaxedHTTPSValidation())))
 	                .param("longitude", 121.04925573429551)
 	                .param("latitude", 31.315590522490712)
 	                .get("https://www.showapi.com/auth/login");
@@ -73,8 +66,6 @@ public class TestCaseOfApiParallel extends WebTestNGBase{
 			boolean result = false;
 			
 	        Response response = given()
-	                // 配置SSL 让所有请求支持所有的主机名
-	                .config((RestAssured.config().sslConfig(new SSLConfig().relaxedHTTPSValidation())))
 	                .param("longitude", 121.04925573429551)
 	                .param("latitude", 31.315590522490712)
 	                .get("https://ditu.amap.com/service/regeo");
@@ -98,8 +89,6 @@ public class TestCaseOfApiParallel extends WebTestNGBase{
 			boolean result = false;
 			
 	        Response response = given()
-	                // 配置SSL 让所有请求支持所有的主机名
-	                .config((RestAssured.config().sslConfig(new SSLConfig().relaxedHTTPSValidation())))
 	                .param("longitude", 121.04925573429551)
 	                .param("latitude", 31.315590522490712)
 	                .get("https://ditu.amap.com/service/regeo");
